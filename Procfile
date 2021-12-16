4e9bacd2c3e6f2028d4bda83f0b230028dc9f638
Gabriel <g.dalrovere@gmail.com>
2021-12-16 11:23:40 -0300

    teste heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn run:create
+web: gunicorn run:app
--- run.py
+++ run.py
@@ -1,9 +1,8 @@
 from app import app, db
 
 
-def create():
-    if __name__ == "__main__":
-        db.create_all()
-        app.run(
-            debug=True
-        )
+if __name__ == "__main__":
+    db.create_all()
+    app.run(
+        debug=True
+    )
